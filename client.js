23dd2eb06a864da0efa583e705a1cf0b57abf5c6
Malcolm McQueen <malcolmmcqueen@live.com>
2022-08-31 21:15:30 +0000

    updated client.js and play.js with require() functions. added conn.write(name) to play.js

--- client.js
+++ client.js
@@ -1,12 +1,21 @@
+const net = require("net");
+
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
     host: 'localhost',
     port: 50541,
   });
+
   // interpret incoming data as text
   conn.on('data', (data) => {
     console.log(data);
+  });
+
+  // function to run when client connects to server
+  conn.on('connect', () => {
+    console.log(`Connected to server`)
+    conn.write(`Name: MMM`);
   });
   conn.setEncoding("utf8");
 
@@ -14,6 +23,4 @@ const connect = function () {
 };
 
 
-module.exports = {
-  connect,
-};
+module.exports = connect
--- play.js
+++ play.js
@@ -1,19 +1,6 @@
 const net = require("net");
+const connect = require("./client.js")
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541,
-  });
-  // interpret incoming data as text
-  conn.on('data', (data) => {
-    console.log(data);
-  });
-  conn.setEncoding("utf8");
-
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
