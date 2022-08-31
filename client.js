2fc5c594f2a22ad6fc8e835afaaeef9b82db7ec4
Malcolm McQueen <malcolmmcqueen@live.com>
2022-08-31 20:04:45 +0000

    created client.js and moved connect function from play.js to client.js

---
+++ client.js
@@ -0,0 +1,19 @@
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541,
+  });
+  // interpret incoming data as text
+  conn.on('data', (data) => {
+    console.log(data);
+  });
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+
+module.exports = {
+  connect,
+};
